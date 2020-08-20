fa14f71c7242d5027f2189c9b1678718ab2b96f2
khanhnq <khanhnq@paxcreation.com>
2020-08-20 09:50:58 +0700

    add fb chat

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-
+import VueFbCustomerChat from 'vue-fb-customer-chat'
 Vue.use(VueI18n)
-
+Vue.use(VueFbCustomerChat, {
+  page_id: "479073292905392", //  change 'null' to your Facebook Page ID,
+  theme_color: '#333333', // theme color in HEX
+  locale: 'vi_VN', // default 'en_US'
+})
 function loadLocaleMessages () {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages = {}
