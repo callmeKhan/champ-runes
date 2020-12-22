5334579189c49e2a7a76fcf28961cf775bdc70b4
CallMeKhan <styleatlove97@gmail.com>
2020-12-22 10:47:50 +0700

    services worker 6

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,43 +3,19 @@ workbox.precaching.precacheAndRoute([]);
 
 // Cache images:
 workbox.routing.registerRoute(
-  /\.(?:png|webp)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
-      })
-    ]
-  })
+  new RegExp('/img/'),
+  new workbox.strategies.CacheFirst()
 );
 
 // Cache css:
 workbox.routing.registerRoute(
-  /\.(?:css)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "css",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
-      })
-    ]
-  })
+  new RegExp('/js/'),
+  new workbox.strategies.CacheFirst()
 );
 
 
 // Cache js:
 workbox.routing.registerRoute(
-  /\.(?:js)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "js",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
-      })
-    ]
-  })
+  new RegExp('/css/'),
+  new workbox.strategies.CacheFirst()
 );
