64d41545d1cf13a4e64db3cd77bac57ed18a79b7
CallMeKhan <styleatlove97@gmail.com>
2020-12-22 09:45:18 +0700

    add service worker

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered () {
+      console.log('Service worker has been registered.')
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updatefound () {
+      console.log('New content is downloading.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+  // public/service-worker.js
+  workbox.setConfig({ debug: true });
+  workbox.precaching.precacheAndRoute([]);
+
+  // Cache images:
+  workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+      })
+    ]
+  })
+  );
+}
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,11 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
+  },
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "public/service-worker.js"
+    }
   }
 }
